494f09052bfcfb64740d5c9fdb7b83eca871e791
YanaFicus <248003852+yanaFic@users.noreply.github.com>
2026-03-20 23:42:02 +0300

    fix: delete 2x BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 
 function App() {
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-    </BrowserRouter>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
 
